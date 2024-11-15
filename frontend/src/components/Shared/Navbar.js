254eabb6f54a46f4ea36549b9d70c9c7fcced286
ranieshM05 <ranieshrajdharan876@gmail.com>
2024-11-15 18:05:48 +0530

    front  is added

--- frontend/src/components/Shared/Navbar.js
+++ frontend/src/components/Shared/Navbar.js
@@ -3,10 +3,39 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About Us</Link>
-            <Link to="/profile">Profile</Link>
+        <nav className="bg-blue-500 p-4">
+            <div className="flex justify-center space-x-6">
+                <Link 
+                    to="/" 
+                    className="text-white text-lg font-semibold hover:text-yellow-300"
+                >
+                    Home
+                </Link>
+                <Link 
+                    to="/about" 
+                    className="text-white text-lg font-semibold hover:text-yellow-300"
+                >
+                    About Us
+                </Link>
+                <Link 
+                    to="/profile" 
+                    className="text-white text-lg font-semibold hover:text-yellow-300"
+                >
+                    Profile
+                </Link>
+                <Link 
+                    to="/booking/:id" 
+                    className="text-white text-lg font-semibold hover:text-yellow-300"
+                >
+                    Booking
+                </Link>
+                <Link 
+                    to="/payment" 
+                    className="text-white text-lg font-semibold hover:text-yellow-300"
+                >
+                    Payment
+                </Link>
+            </div>
         </nav>
     );
 };
