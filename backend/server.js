08d90be6a5de3ecb9ae4ec073c8557d8c4e29c0f
ranieshM05 <ranieshrajdharan876@gmail.com>
2024-11-29 15:32:24 +0530

    re file is added

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,5 @@ app.use("/api/movies", moviesRoutes);
 // Error Handling Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
